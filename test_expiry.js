fa1369586b53040257382f2cf10ea0e6ac4c3ade
sri-pusuluri <sri.pusuluri@gmail.com>
2026-07-22 23:17:08 +0530

    Implement responsive navigation menu bar with mobile hamburger switch

--- test_expiry.js
+++ test_expiry.js
@@ -1,5 +1,5 @@
 const http = require('http');
-http.get('http://localhost:3000/api/options/chain?symbol=NSE:NIFTY50-INDEX', (res) => {
+http.get('http://localhost:5173/api/options/chain?symbol=NSE:NIFTY50-INDEX', (res) => {
     let raw = '';
     res.on('data', c => raw += c);
     res.on('end', () => {
